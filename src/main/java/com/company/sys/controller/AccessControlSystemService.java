e60317b9bc1ffb1766c49dabf0c944ebe9c28d25
fgvfgjh <3400130344@qq.com>
2023-11-24 19:31:14 +0800

    11

--- src/main/java/com/company/sys/controller/AccessControlSystemService.java
+++ src/main/java/com/company/sys/controller/AccessControlSystemService.java
@@ -2,5 +2,6 @@ package com.company.sys.controller;
 
 public class AccessControlSystemService {
     public void syncAccessInfo(AccessControlSystemService accessControlSystemService) {
+        
     }
 }
